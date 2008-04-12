50a1837008c2658864ae8cb8ffb216de190bc2c7
Jakub Lužný <limoto94@gmail.com>
2008-04-12 18:28:17 +0300

    Fix undefined PATH_MAX with GCC 4.3.

--- hooks.cpp
+++ hooks.cpp
@@ -22,6 +22,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include <fstream>
 #include <sys/mman.h>
 #include <cstdlib>
+#include <limits.h>
 
 #ifdef __SDL
 #include <SDL/SDL.h>
